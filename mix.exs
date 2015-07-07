bcb271fcf49784c975065d19e8df17f5aa9a4ef5
Tyler Cross <tyler@crosscollab.com>
2015-07-07 17:59:40 -0400

    Use https git uri for meck dependency

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Bandwidth.Mixfile do
       { :poison, "~> 1.4.0", override: true },
       { :dialyze, "~> 0.1.4", only: [:dev, :test] },
       { :ex_spec, "~> 0.3.0", only: :test },
-      { :meck, git: "git@github.com:eproxus/meck.git", tag: "0.8.3", only: :test },
+      { :meck, git: "https://github.com/eproxus/meck.git", tag: "0.8.3", only: :test },
       { :coverex, "~> 1.3.0", only: :test}]
   end
 end
