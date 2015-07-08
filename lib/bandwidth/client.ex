fce667004f0056969bb8bfe31cddc8559a40f832
Tyler Cross <tyler@crosscollab.com>
2015-07-08 12:55:57 -0400

    Reduce API surface area of client.
    This commit makes certain functions private
    that were previously public. The testing has
    also been improved to cover each of the request
    helper methods.
    
    I'm seeing unexpected coverage data and think that
    there is somethign going on causing false negatives
    on line coverage.

--- lib/bandwidth/client.ex
+++ lib/bandwidth/client.ex
@@ -6,6 +6,7 @@ defmodule Bandwidth.Client do
   @type t              :: %Client{user_id: binary, api_token: binary, api_secret: binary, endpoint: binary}
   @type response       :: {integer, any, [header]}
   @type result         :: {:ok, response} | {:error, binary}
+  @type body           :: any
   @type header         :: {binary, binary}
   @type path_segments  :: binary | [ binary ]
   @type request_method :: :get | :put | :post | :delete | :put | :patch | :head
@@ -15,24 +16,8 @@ defmodule Bandwidth.Client do
     %Client{user_id: user_id, api_token: api_token, api_secret: api_secret}
   end
 
-  @spec url(t, path_segments) :: binary
-  def url(%Client{endpoint: endpoint}, path_segments) do
-    path = List.flatten([ path_segments ])
-    Path.join([endpoint, Path.join(Enum.map path, &to_string/1)])
-  end
-
-  @spec add_headers(t, [header]) :: [header]
-  def add_headers(%Client{api_token: api_token, api_secret: api_secret}, headers) do
-    credentials = "#{api_token}:#{api_secret}"
-    [
-      {"Accept", "application/json"},
-      {"Content-Type", "application/json"},
-      {"Authorization", "Basic #{:base64.encode(credentials)}"}
-    ] ++ headers
-  end
-
-  @spec request(t, request_method, path_segments, binary, [header]) :: result
-  def request(client, method, path_segments, body \\ "", headers \\ []) do
+  @spec request(t, request_method, path_segments, body, [header]) :: result
+  def request(client, method, path_segments, body \\ nil, headers \\ []) do
     case HTTPoison.request(method, url(client, path_segments), process_request_body(body), add_headers(client, headers)) do
       {:ok, response} -> {:ok, process_response(response)}
       {:error, error} -> {:error, error.reason}
@@ -44,8 +29,8 @@ defmodule Bandwidth.Client do
     request(client, :get, path_segments)
   end
 
-  @spec post_resource(t, path_segments, binary) :: result
-  def post_resource(client, path_segments, body \\ "") do
+  @spec post_resource(t, path_segments, body) :: result
+  def post_resource(client, path_segments, body \\ nil) do
     request(client, :post, path_segments, body)
   end
 
@@ -59,8 +44,8 @@ defmodule Bandwidth.Client do
     get_resource(client, ["users", user_id, path_segments])
   end
 
-  @spec post_user_resource(t, path_segments, binary) :: result
-  def post_user_resource(client = %Client{user_id: user_id}, path_segments, body \\ "") do
+  @spec post_user_resource(t, path_segments, body) :: result
+  def post_user_resource(client = %Client{user_id: user_id}, path_segments, body \\ nil) do
     post_resource(client, ["users", user_id, path_segments], body)
   end
 
@@ -69,22 +54,40 @@ defmodule Bandwidth.Client do
     delete_resource(client, ["users", user_id, path_segments])
   end
 
+  @spec url(t, path_segments) :: binary
+  defp url(%Client{endpoint: endpoint}, path_segments) do
+    path = List.flatten([ path_segments ])
+    Path.join([endpoint, Path.join(Enum.map path, &to_string/1)])
+  end
+
+  @spec add_headers(t, [header]) :: [header]
+  defp add_headers(%Client{api_token: api_token, api_secret: api_secret}, headers) do
+    credentials = "#{api_token}:#{api_secret}"
+    [
+      {"Accept", "application/json"},
+      {"Content-Type", "application/json"},
+      {"Authorization", "Basic #{:base64.encode(credentials)}"}
+    ] ++ headers
+  end
+
   @spec process_response(HTTPoison.Response.t) :: result
-  def process_response(response) do
-    headers = response.headers
-    status_code = response.status_code
+  defp process_response(response) do
     body = response.body
 
     processed_body = case body do
       ""                     -> nil
-      _ when is_binary(body) -> Poison.decode! body
+      _ when is_binary(body) -> case Poison.decode body do
+                                  {:ok, body} -> body
+                                  {:error, _} -> nil
+                                end
       _                      -> nil
     end
 
-    {status_code, processed_body, headers}
+    {response.status_code, processed_body, response.headers}
   end
 
-  def process_request_body(body) do
+  @spec process_response(body) :: body
+  defp process_request_body(body) do
     case body do
       ""  -> ""
       nil -> ""
--- test/client_test.exs
+++ test/client_test.exs
@@ -9,6 +9,12 @@ defmodule Bandwidth.ClientTest do
   @test_api_token "test-auth-token"
   @test_api_secret "test-auth-secret"
   @default_endpoint "https://test-endpoint.com"
+  @default_credentials "#{@test_api_token}:#{@test_api_secret}"
+  @default_headers [
+    {"Accept", "application/json"},
+    {"Content-Type", "application/json"},
+    {"Authorization", "Basic #{:base64.encode(@default_credentials)}"}
+  ]
 
   setup_all do
     :meck.new :hackney, [:no_link]
@@ -41,145 +47,142 @@ defmodule Bandwidth.ClientTest do
     end
   end
 
-  describe "getting a resource url" do
-    context "when the client endpoint has a trailing slash" do
-      it "returns the correct url" do
-        client = create_client(endpoint: "http://test.com/")
-        path = [ "items", 45 ]
-        url = url(client, path)
-        assert url === "#{client.endpoint}items/45"
-      end
-    end
-
-    context "when the client endpoint does not have a trailing slash" do
-      it "returns the correct url" do
-        client = create_client(endpoint: "http://test.com")
-        path = [ "items", 45 ]
-        url = url(client, path)
-        assert url === "#{client.endpoint}/items/45"
-      end
-    end
-
-    context "when the path has non-string items" do
-      it "returns the correct url" do
-        client = create_client
-        path = [ "items", 45 ]
-        url = url(client, path)
-        assert url === "#{client.endpoint}/items/45"
-      end
-    end
-
-    context "when the path has all string items" do
-      it "returns the correct url" do
-        client = create_client
-        path = [ "items", "foo" ]
-        url = url(client, path)
-        assert url === "#{client.endpoint}/items/foo"
-      end
-    end
-  end
-
-  describe "adding headers" do
-    setup_all do
-      {:ok, headers: add_headers(create_client, [])}
-    end
-
-    it "adds an accept header for json", context do
-      assert Enum.any?(context[:headers], &(&1 === {"Accept", "application/json"}))
-    end
-
-    it "adds a content type header for json", context do
-      assert Enum.any?(context[:headers], &(&1 === {"Content-Type", "application/json"}))
-    end
-
-    it "adds an authorization header", context do
-      credentials = "#{@test_api_token}:#{@test_api_secret}"
-      assert Enum.any?(context[:headers], &(&1 === {"Authorization", "Basic #{:base64.encode(credentials)}"}))
-    end
-  end
-
-  describe "processing a request body" do
-    context "with valid json" do
-      it "returns the encoded body" do
-        assert process_request_body(%{test: "data"}) === "{\"test\":\"data\"}"
-      end
-    end
-
-    context "with invalid json" do
-      it "returns the body as is" do
-        assert process_request_body("4") === "\"4\""
-      end
-    end
-
-    context "that is empty or nil" do
-      it "returns nil" do
-        assert process_request_body("")  === ""
-        assert process_request_body(nil) === ""
-      end
-    end
-  end
-
-  describe "processing a response" do
-    context "for a failed request" do
-      context "with a body" do
-        it "parses the body" do
-          assert process_response(%HTTPoison.Response{status_code: 404, body: "{\"test\": 4}"}) === {404, %{"test" => 4}, []}
-        end
-      end
-
-      context "without a body" do
-        it "doesn't parse the body" do
-          assert process_response(%HTTPoison.Response{status_code: 404})           === {404, nil, []}
-          assert process_response(%HTTPoison.Response{status_code: 404, body: ""}) === {404, nil, []}
-        end
-      end
-    end
-
-    context "for a successful request" do
-      context "with a body" do
-        it "parses the body" do
-          assert process_response(%HTTPoison.Response{status_code: 200, body: "{\"test\": 4}"}) === {200, %{"test" => 4}, []}
-        end
-      end
-
-      context "without a body" do
-        it "doesn't parse the body" do
-        assert process_response(%HTTPoison.Response{status_code: 200})             === {200, nil, []}
-          assert process_response(%HTTPoison.Response{status_code: 200, body: ""}) === {200, nil, []}
-        end
-      end
-    end
-  end
-
   describe "making a request" do
-    setup_all do
+    setup do
       :meck.new HTTPoison, [:no_link]
-      :meck.expect HTTPoison, :request, fn
-        method, _, _, _ when method === :get    -> {:ok, %HTTPoison.Response{status_code: 200}}
-        method, _, _, _ when method === :post   -> {:ok, %HTTPoison.Response{status_code: 200}}
-        method, _, _, _ when method === :delete -> {:ok, %HTTPoison.Response{status_code: 200}}
-      end
       on_exit fn ->
         :meck.unload HTTPoison
       end
       :ok
     end
 
-    context "with the get helper" do
-      it "uses the correct parameters" do
-        assert get_resource(create_client, "test") === {:ok, {200, nil, []}}
+    context "successfully" do
+      it "returns an ok tuple" do
+        args = [:get, "#{@default_endpoint}/users/#{@test_user_id}/test", "", @default_headers]
+        :meck.expect HTTPoison, :request, [{args, {:ok, %HTTPoison.Response{status_code: 200}}}]
+        assert get_user_resource(create_client, "test") === {:ok, {200, nil, []}}
+        assert :meck.num_calls(HTTPoison, :request, args) === 1
+        assert :meck.validate HTTPoison
+        :meck.delete HTTPoison, :request, 4
       end
     end
 
-    context "with the post helper" do
-      it "uses the correct parameters" do
-        assert post_resource(create_client, "test") === {:ok, {200, nil, []}}
+    context "resulting in an error" do
+      it "returns an error tuple" do
+        reason = "some-error-happened"
+        args = [:get, "#{@default_endpoint}/users/#{@test_user_id}/test", "", @default_headers]
+        :meck.expect HTTPoison, :request, [{args, {:error, %HTTPoison.Error{reason: reason}}}]
+        assert get_user_resource(create_client, "test") === {:error, reason}
+        assert :meck.num_calls(HTTPoison, :request, args) === 1
+        assert :meck.validate HTTPoison
+        :meck.delete HTTPoison, :request, 4
       end
     end
 
-    context "with the delete helper" do
-      it "uses the correct parameters" do
-        assert delete_resource(create_client, "test") === {:ok, {200, nil, []}}
+    context "for a user resource" do
+      context "with the get_user_resource helper" do
+        it "sends the correct request" do
+          args = [:get, "#{@default_endpoint}/users/#{@test_user_id}/test", "", @default_headers]
+          :meck.expect HTTPoison, :request, [{args, {:ok, %HTTPoison.Response{status_code: 200}}}]
+          assert get_user_resource(create_client, "test") === {:ok, {200, nil, []}}
+          assert :meck.num_calls(HTTPoison, :request, args) === 1
+          assert :meck.validate HTTPoison
+          :meck.delete HTTPoison, :request, 4
+        end
+      end
+
+      context "with the post_user_resource helper" do
+        it "sends the correct request" do
+          args = [:post, "#{@default_endpoint}/users/#{@test_user_id}/test", "{}", @default_headers]
+          :meck.expect HTTPoison, :request, [{args, {:ok, %HTTPoison.Response{status_code: 200}}}]
+          assert post_user_resource(create_client, "test", %{}) === {:ok, {200, nil, []}}
+          assert :meck.num_calls(HTTPoison, :request, args) === 1
+          assert :meck.validate HTTPoison
+          :meck.delete HTTPoison, :request, 4
+        end
+      end
+
+      context "with the delete_user_resource helper" do
+        it "sends the correct request" do
+          args = [:delete, "#{@default_endpoint}/users/#{@test_user_id}/test", "", @default_headers]
+          :meck.expect HTTPoison, :request, [{args, {:ok, %HTTPoison.Response{status_code: 200}}}]
+          assert delete_user_resource(create_client, "test") === {:ok, {200, nil, []}}
+          assert :meck.num_calls(HTTPoison, :request, [:delete, "#{@default_endpoint}/users/#{@test_user_id}/test", "", @default_headers]) === 1
+          assert :meck.validate HTTPoison
+          :meck.delete HTTPoison, :request, 4
+        end
+      end
+    end
+
+    context "for a resource" do
+      context "with the get_resource helper" do
+        it "sends the correct request" do
+          args = [:get, "#{@default_endpoint}/test", "", @default_headers]
+          :meck.expect HTTPoison, :request, [{args, {:ok, %HTTPoison.Response{status_code: 200}}}]
+          assert get_resource(create_client, "test") === {:ok, {200, nil, []}}
+          assert :meck.num_calls(HTTPoison, :request, args) === 1
+          assert :meck.validate HTTPoison
+          :meck.delete HTTPoison, :request, 4
+        end
+      end
+
+      context "with the post_resource helper" do
+        context "with no body" do
+          it "sends the correct request" do
+            args = [:post, "#{@default_endpoint}/test", "{}", @default_headers]
+            :meck.expect HTTPoison, :request, [{args, {:ok, %HTTPoison.Response{status_code: 200}}}]
+            assert post_resource(create_client, "test", %{}) === {:ok, {200, nil, []}}
+            assert :meck.num_calls(HTTPoison, :request, args) === 1
+            assert :meck.validate HTTPoison
+            :meck.delete HTTPoison, :request, 4
+          end
+        end
+      end
+
+      context "with the delete_resource helper" do
+        it "sends the correct request" do
+          args = [:delete, "#{@default_endpoint}/test", "", @default_headers]
+          :meck.expect HTTPoison, :request, [{args, {:ok, %HTTPoison.Response{status_code: 200}}}]
+          assert delete_resource(create_client, "test") === {:ok, {200, nil, []}}
+          assert :meck.num_calls(HTTPoison, :request, args) === 1
+          assert :meck.validate HTTPoison
+          :meck.delete HTTPoison, :request, 4
+        end
+      end
+    end
+
+    context "with a empty body response" do
+      it "sets the result body to nil" do
+        args = [:get, "#{@default_endpoint}/users/#{@test_user_id}/test", "", @default_headers]
+        :meck.expect HTTPoison, :request, [{args, {:ok, %HTTPoison.Response{status_code: 200, body: ""}}}]
+        assert get_user_resource(create_client, "test") === {:ok, {200, nil, []}}
+        assert :meck.num_calls(HTTPoison, :request, args) === 1
+        assert :meck.validate HTTPoison
+        :meck.delete HTTPoison, :request, 4
+      end
+    end
+
+    context "with a json response" do
+      context "containing valid json" do
+        it "decodes the json" do
+          args = [:get, "#{@default_endpoint}/users/#{@test_user_id}/test", "", @default_headers]
+          :meck.expect HTTPoison, :request, [{args, {:ok, %HTTPoison.Response{status_code: 200, body: "{\"foo\": \"bar\"}"}}}]
+          assert get_user_resource(create_client, "test") === {:ok, {200, %{"foo" => "bar"}, []}}
+          assert :meck.num_calls(HTTPoison, :request, args) === 1
+          assert :meck.validate HTTPoison
+          :meck.delete HTTPoison, :request, 4
+        end
+      end
+
+      context "containing invalid json" do
+        it "returns nil" do
+          args = [:get, "#{@default_endpoint}/users/#{@test_user_id}/test", "", @default_headers]
+          :meck.expect HTTPoison, :request, [{args, {:ok, %HTTPoison.Response{status_code: 200, body: "{\"f \"bar\"}"}}}]
+          assert get_user_resource(create_client, "test") === {:ok, {200, nil, []}}
+          assert :meck.num_calls(HTTPoison, :request, args) === 1
+          assert :meck.validate HTTPoison
+          :meck.delete HTTPoison, :request, 4
+        end
       end
     end
   end
