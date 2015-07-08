6a573279519f727123772b4d11365b208f5147b6
Tyler Cross <tyler@crosscollab.com>
2015-07-08 13:01:25 -0400

    Fix dependency version issue.
    The dependency on coverex for testing was
    causing a version conflict with this projects
    dependencies. This commit makes bandwidth
    (temporarily) depend on the fork of coverex
    that has up-to-date versions.

--- mix.exs
+++ mix.exs
@@ -23,11 +23,11 @@ defmodule Bandwidth.Mixfile do
   end
 
   defp deps do
-    [ { :httpoison, "~> 0.7.0", override: true },
-      { :poison, "~> 1.4.0", override: true },
+    [ { :httpoison, "~> 0.7.0" },
+      { :poison, "~> 1.4.0" },
       { :dialyze, "~> 0.1.4", only: [:dev, :test] },
       { :ex_spec, "~> 0.3.0", only: :test },
       { :meck, github: "eproxus/meck", tag: "0.8.3", only: :test },
-      { :coverex, "~> 1.3.0", only: :test}]
+      { :coverex, github: "wtcross/coverex", branch: "bump-dependency-versions", only: :test}]
   end
 end
